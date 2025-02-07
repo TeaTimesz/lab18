6d53212c331aa8601e48a9393d0922c40bcda4d2
TeaTimesz <rawiphatmak@gmail.com>
2025-02-07 21:20:31 +0700

    completed

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,15 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect R1,Rect R2){
+	Rect Overlap;
+	Overlap.x = max(R1.x,R2.x);
+	Overlap.y = min(R1.y,R2.y);
+	Overlap.w = min(R1.x + R1.w,R2.x + R2.w) - Overlap.x;
+	Overlap.h = Overlap.y - max(R1.y - R1.h,R2.y - R2.h);
 
-
-
+	if(Overlap.w > 0 && Overlap.h > 0){
+		return Overlap.h * Overlap.w;
+	}
+	return 0;
 }
